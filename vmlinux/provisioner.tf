55e6c98c8cbb37fdf37726a11803daa410721477
ganesh077 <ganeshthampi077.gmail.com>
2023-04-19 00:13:57 -0400

    working

--- vmlinux/provisioner.tf
+++ vmlinux/provisioner.tf
@@ -14,6 +14,10 @@ resource "null_resource" "display_hostnames" {
 }
 
  provisioner "local-exec" {
+    command = "ssh-keyscan ${azurerm_public_ip.pip[count.index+1].ip_address} >> ~/.ssh/known_hosts"
+  }
+
+ provisioner "local-exec" {
     command = "ansible-playbook -i '${azurerm_public_ip.pip[count.index+1].ip_address},' -u ${var.admin_username} --private-key=${var.ssh_private_key} ansible/groupX-playbook.yaml"
      
 }
